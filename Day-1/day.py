74f5b827d1e3319e6385b5e8c6a428b61c0ff507
Ganesh20000 <ganeshkarli19@gmail.com>
2025-11-22 19:51:46 +0530

    add first commit

---
+++ Day-1/day.py
@@ -0,0 +1,12 @@
+# Input from user
+sentence = input("Enter a sentence: ")
+
+# Replace spaces with underscoresd
+modified_sentence = sentence.replace(" " , "_")
+modified_sentence2=modified_sentence.replace("|","_")
+
+# Output the result
+print("Modified sentence:", modified_sentence2)
+# print("_")*1000
+# update this
+
