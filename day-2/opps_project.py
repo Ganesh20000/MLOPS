d80b1f155270a66bda5cceab924324f91368219b
Ganesh20000 <ganeshkarli19@gmail.com>
2025-11-23 22:49:38 +0530

    oop project

---
+++ day-2/opps_project.py
@@ -0,0 +1,34 @@
+
+class  facebook:
+
+    def __init__(self):
+        self.username=" "
+        self.password=" "
+        self.loggedin=" "
+        self.manu()
+
+
+    def manu(self):
+        user_input=input(""" welcome to facebook how would like to  proceed ?
+                         1.press one for signup
+                         2.Press two for singin 
+                         3.press three for write a post
+                         4.press four for message a friend
+                         5.press any key to exit""")
+        if user_input =="1":
+            pass
+        if user_input =="2":
+            pass
+        if user_input =="3":
+            pass
+        if user_input=="4":
+            pass
+        if user_input=="5":
+            exit()
+
+
+
+
+
+
+d=facebook()
